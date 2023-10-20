514baed30bcf811f155abcf17fa83436562b0123
PieMenard <pinedo.m.gustavo@gmail.com>
2023-10-20 19:56:14 -0400

    get products from database

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 import express from "express";
-import products from "./data/products.js";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 dotenv.config();
 connectDB();
+import router from "./routes/productRoutes.js";
 
-const port = process.env.PORT||5000;
+const port = process.env.PORT || 5000;
 
 const app = express();
 
@@ -13,14 +13,7 @@ app.get("/", (req, res) => {
   res.send("API is running...");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", router);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
